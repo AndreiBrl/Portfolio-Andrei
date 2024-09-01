c8fe7d326836502129d127f54b1ed7f015cef428
Andrei <barbutodev@gmail.com>
2024-09-01 19:35:50 -0300

    estrutura geral criada

--- portfolio-andrei/src/app/page.js
+++ portfolio-andrei/src/app/page.js
@@ -6,12 +6,122 @@ export default function Home() {
 
     return (
         <div className={styles.container}>
-
             <div className={styles.sub_container}>
-                <h1>Andrei Barbuto</h1>
+                {/* <div>
+                    <Image
+                        src='/img/eu.png'
+                        alt='Logo'
+                        width={200}
+                        height={200}
+                        className={styles.imgPerfil}
+                    ></Image>
+                </div> */}
+                <div>
+                    <Image
+                        src='/img/Aimg.png'
+                        alt='Logo'
+                        width={200}
+                        height={200}
+                        className={styles.aImg}
+                    ></Image>
+                    <div>
+                        <div>
+
+                            <h1> Bem vindo, Visitante</h1>
+                        </div>
+                        <p>Aqui compartilho todas as minhas criações</p>
+                    </div>
+                    <Image
+                        src='/img/katana.png'
+                        alt='Logo'
+                        width={200}
+                        height={200}
+                        className={styles.katana}
+                    ></Image>
+                </div>
             </div>
 
             <div className={styles.sub_container}>
+                <h1 className={styles.title1}>Andrei Barbuto.</h1>
+                <div>
+                    <div>
+                        <div className={styles.teste11}>   <Image
+                            src='/img/git.png'
+                            alt='Logo'
+                            width={200}
+                            height={200}
+                            className={styles.menuRedesGit}
+                        ></Image></div>
+                        <div className={styles.teste11}><Image
+                            src='/img/wpp.png'
+                            alt='Logo'
+                            width={200}
+                            height={200}
+                            className={styles.menuRedesWpp}
+                        ></Image></div>
+                        <div className={styles.teste11}><Image
+                            src='/img/linkedin.png'
+                            alt='Logo'
+                            width={200}
+                            height={200}
+                            className={styles.menuRedesWpp}
+                        ></Image></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div>
+
+                        <h2 className={styles.title1}>バルブト アンドレイ</h2>
+                        <h2 className={styles.title1}>Desenvolvedor Web.</h2>
+                        <h2 className={styles.title1}>Designer.</h2>
+                        <h2 className={styles.title1}>Escritor.</h2>
+                    </div>
+
+                </div>
+                    <h1 >Projetos</h1>
+                <div>
+                    <div className={styles.box}>
+
+                        <Image
+                            src='/img/teste.jpeg'
+                            alt='Logo'
+                            width={200}
+                            height={200}
+                            className={styles.boxImage}
+                        >
+
+                        </Image>
+                    </div>
+                    <div className={styles.box}>
+
+                        <Image
+                            src='/img/teste.jpeg'
+                            alt='Logo'
+                            width={200}
+                            height={200}
+                            className={styles.boxImage}
+                        >
+
+                        </Image>
+                    </div>
+                    <div className={styles.box}>
+
+                        <Image
+                            src='/img/gorila.jpeg'
+                            alt='Logo'
+                            width={200}
+                            height={200}
+                            className={styles.boxImage}
+                        >
+
+                        </Image>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className={styles.sub_container}>
 
                 <div className={styles.box}>
 
@@ -49,43 +159,8 @@ export default function Home() {
 
                     </Image>
                 </div>
-                
-                <div className={styles.box}>
 
-                    <Image
-                        src='/img/teste.jpeg'
-                        alt='Logo'
-                        width={200}
-                        height={200}
-                        className={styles.boxImage}
-                    >
 
-                    </Image>
-                </div>
-                <div className={styles.box}>
-
-                    <Image
-                        src='/img/teste.jpeg'
-                        alt='Logo'
-                        width={200}
-                        height={200}
-                        className={styles.boxImage}
-                    >
-
-                    </Image>
-                </div>
-                <div className={styles.box}>
-
-                    <Image
-                        src='/img/teste.jpeg'
-                        alt='Logo'
-                        width={200}
-                        height={200}
-                        className={styles.boxImage}
-                    >
-
-                    </Image>
-                </div>
 
             </div>
         </div>
