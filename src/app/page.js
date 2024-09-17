2d32ddfb901fd5ae856e2c0f64acfa90cd9d7230
Andrei <barbutodev@gmail.com>
2024-09-17 19:06:26 -0300

    teste imagem com qualidade100

--- src/app/page.js
+++ src/app/page.js
@@ -338,6 +338,7 @@ export default function Home() {
                         alt='Logo'
                         width={300}
                         height={300}
+                        quality={100}
                         className={styles.imgMen}
                     ></Image>
                     <div>
